a7908fc1975723648715853939a0abef64c9283b
gutovesco <guto.vesco@hotmail.com>
2020-11-16 19:17:13 -0300

    index de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category")
+const Article = require("./Articles")
+const slugify = require("slugify")
 
-router.get("/articles", (req, res) =>{
+router.get("/admin/articles", (req, res) =>{
     res.send("Rota de Artigos");
 });
 
 router.get("/admin/articles/new", (req, res) =>{
-    res.render("admin/articles/new");
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    })
 });
+
+router.post("/articles/save", (req, res) => {
+    var {title, body, category} = req.body;
+
+    Article.create({
+        title,
+        slug: slugify(title),
+        body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
+})
 
 module.exports = router;
